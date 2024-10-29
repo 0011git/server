59024dcf3ce837c74262886607d305961402949b
JY Sung <0125sjy@gmail.com>
2024-10-29 19:41:56 +0900

    news api

---
+++ api/news.js
@@ -0,0 +1,54 @@
+//vue.js news(딥서치뉴스 api)용 서버
+
+const express = require('express')  //express import해오기
+const news = express.Router() //express router 모듈
+const axios = require('axios');
+
+news.get('/', async function(req, res) {
+
+    /** 날짜 형식 바꾸는 함수 */
+    function dateFormat(date) {
+        const yyyy = date.getFullYear();
+        const mm = String(date.getMonth() + 1).padStart(2, "0"); // 월은 0부터 시작하므로 +1 필요
+        const dd = String(date.getDate()).padStart(2, "0");
+        return `${yyyy}-${mm}-${dd}`;
+    }    
+    const today = new Date();
+    const formattedToday = dateFormat(today);
+
+    /** 키워드 형식 바꾸는 함수 */
+    function keywordFormat(raw) {
+        return raw.split(' ').join(' OR ');
+    }
+    const formattedKeyword = keywordFormat('인공 지능 특이점')
+    //검색에는 날짜 필요x, 키워드랑 api키만 있어도 됨.
+
+    
+
+    const localBaseUrl = 'https://api-v2.deepsearch.com/v1/articles'
+    const globalBaseUrl = 'https://api-v2.deepsearch.com/v1/global-articles'
+    const sectionArr = [politics, economy, society, culture, world, tech, entertainment, opinion]
+    const sectionBaseUrl = `${localBaseUrl}/${sectionArr[0]}`
+    const defaultParams = {
+        api_key: '2e54407962f548d698a9e69f703b0bad',
+    }
+    const todayParams = {        
+        date_from: formattedToday,
+        date_to: formattedToday,
+        groupby: 'publisher',  //언론사별
+        page_size: 10,    //상위 10개
+    }
+    const searchParams = {
+        keyword: formattedKeyword,
+    }
+
+    // 'https://api-v2.deepsearch.com/v1/articles?date_from=2024-10-23&date_to=2024-10-24&api_key=2e54407962f548d698a9e69f703b0bad'
+    const localRes = await axios.get(localBaseUrl, {params: params});
+    res.json(localRes.data)
+    
+    // const globalRes = await axios.get(globalBaseUrl, {params: params});
+    // res.json(globalRes.data)
+
+})
+
+module.exports = news;
--- api/todos.js
+++ api/todos.js
@@ -2,6 +2,29 @@
 const express = require('express')  //express import해오기
 const todos = express.Router() //express router 모듈
 
+/**Mongo DB 몽고 DB 서버 생성하기
+#몽고DB
+1. database 프로젝트 생성
+2. browse connect
+3. database 생성
+4. collection 생성
+
+#서버
+1. npm i mongodb(설치)
+2. https://www.npmjs.com/package/mongodb 예제코드 확인(find, insert, update, delete)
+3. 코드 수정
+
+#Vercel(배포)
+1. 서버 배포
+2. 인테그레이션(integration): 몽고 디비 접근 설정
+3. setting: 서버 접속 국가 변경
+
+
+#프론트
+1. database - Browse Collection 클릭
+2. 새로고침 하면서 DB 인앤아웃 확인
+*/
+
 
 //몽고db import
 const { MongoClient } = require('mongodb'); //메소드 import
@@ -10,16 +33,14 @@ const url = 'mongodb+srv://0125sjy:OPMqjdMbJFSTF04F@cluster0.s86pi.mongodb.net/?
 const client = new MongoClient(url);
 const dbName = 'todos'; //내가 설정한 db이름
 
-async function mongoConnect() { //db여는 함수
+async function mongoConnect() { //몽고db여는 함수
   await client.connect();
   const db = client.db(dbName);
   const collection = db.collection('data'); //todos 하위의 data에 접근
-  return collection;
+  return;
   // const findResult = await collection.find({}).toArray();
   // res.send(findResult)
 }
-
-
 
 
 //전체 데이터 가져오기(GET)-------------------------------------------------------------------------
@@ -27,55 +48,61 @@ todos.get('/', async function (req, res) {  //req: request, res:response
     //내 로컬호스트 data.json파일(fs.용)
     // res.send(parsedData)
 
-    //몽고db
+    //몽고db => mongoConnect 함수로 만들어둠
     // await client.connect();
     // console.log('Connected successfully to server');
     // const db = client.db(dbName);
-    // const collection = db.collection('data');      => mongoConnect 함수로 만들어둠
+    // const collection = db.collection('data');     
   //----------------------------------------------------------------------
-
+    //몽고db : find
     const collection = await mongoConnect();  //db접속
     const findResult = await collection.find({}).toArray(); //db가져오기
-    client.close(); //서버 끊기
+    // client.close(); //서버 끊기 : 속도 느려서 서버 닫지 않음
     res.send(findResult);
 
 })
 //id가 일치하는 데이터만 가져오기(GET)-------------------------------------------------------------------------
 todos.get('/:id', async function (req, res) {   //루트 디렉토리의 주소 뒷부분(http://localhost:4000//뒷부분)을 받아서 id변수에 저장
-    let {id} = req.params;    //req.params에 주소로 쓸 변수값이 들어옴. {id: 값} 형태임.
+    //내 로컬호스트 data.json파일(fs.용)
+    // let {id} = req.params;    //req.params에 주소로 쓸 변수값이 들어옴. {id: 값} 형태임.
     // let body = parsedData.list.filter((obj) => obj.id == id);  
     // //obj.id는 json에서 뽑아온 number타입, id는 리퀘스트로 받아온 string타입
     // res.send({list:body});   //전체 data.json 보내줌
-  //----------------------------------------------------------------------
-
+  //---------------------------------------------------
+    //몽고db : find
+    let {id} = req.params;    //req.params에 주소로 쓸 변수값이 들어옴. {id: 값} 형태임.
     const collection = await mongoConnect();
     const findResult = await collection.find(id).toArray();
-    client.close(); //서버 끊기
+    // client.close(); //서버 끊기
     res.send(findResult);
 
 })
 // 데이터 추가하기(POST)-------------------------------------------------------------------------
 todos.post('/', async function (req, res) {
-    // let body = {list:[...parsedData.list, req.body]};   // {list:[... 원래 json형식 유지용
-    // let body = [req.body, ...parsedData.list];      //이 코드는 아래 res.send에서 {list:body}로 원래 json형식을 유지하고 있음
+    //내 로컬호스트 data.json파일(fs.용)
+    // let body = {list:[...parsedData.list, req.body]};   // (1) {list:[... 원래 json형식 유지용
+    // let body = [req.body, ...parsedData.list];         //  (2) 이 코드는 아래 res.send에서 {list:body}로 원래 json형식을 유지하고 있음
+                                                  // ====>>>(1)이랑 (2)랑 같은거!
     // fs.writeFileSync('./db/data.json', JSON.stringify({list:body}));
     // console.log(body);
     // res.send(req.body);  //수정한 데이터만 보내줌
     // res.send({list:body}) //수정된 전체 data.json 보내줌
-  //----------------------------------------------------------------------
+  //-------------------------------------------------
+  //몽고db : insertOne
     const collection = await mongoConnect();
     await collection.insertOne(req.body);  //추가한 데이터 저장
     const findResult = await collection.find({}).toArray();  //수정된 데이터 다시 가져와서 저장
-    client.close(); //서버 끊기
+    // client.close(); //서버 끊기
     res.send(findResult); //서버로 보내기
 })
 
 // 데이터 수정하기(PUT)-------------------------------------------------------------------------
 todos.put('/', async function (req, res) {
+  //내 로컬호스트 data.json파일(fs.용)
   // let {id, status} = req.body; //리퀘스트로 받아온 id, name값
   // // let body = parsedData.list.map((obj) => {    //←이 코드랑
   // let body = [...parsedData.list].map((obj) => {  //←이 코드랑 동일!
-  //   if(obj.id == id){ //obj.id는 json에서 뽑아온 number타입, id는 리퀘스트로 받아온 string타입
+  //   if(obj.id == id){ //obj.id는 json에서 뽑아온 number타입, id는 req로 받아온 string타입
   //     obj.status = status;
   //   }
   //   return obj;
@@ -84,17 +111,20 @@ todos.put('/', async function (req, res) {
   // // console.log(body);
   // // res.send(req.body)  //수정한 데이터만 보내줌
   // res.send({list:body})  //수정된 전체 data.json 보내줌
-//----------------------------------------------------------------------
-
+//----------------------------------------------------{id:li.id, status:!li.status}
+//몽고db : updateOne
   const collection = await mongoConnect();
-  await collection.updateOne({id:req.body.id}, {$set:req.body}).toArray();  //데이터 수정{찾을id, 인덱스값}, {수정할값}
+  await collection.updateOne({id:req.body.id}, {$set:req.body.status});  //데이터 수정{찾을id, 인덱스값}, {수정할값}
   const findResult = await collection.find({}).toArray();  //다시 데이터 가져와서 저장
-  client.close(); //서버 끊기
+  // client.close(); //서버 끊기
+
   res.send(findResult); //서버로 보내기
 })
 
+
 //데이터 삭제하기(DELETE)-------------------------------------------------------------------------
 todos.delete('/', async function (req, res) {
+  //내 로컬호스트 data.json파일(fs.용)
   //DELETE는 
   // let {id} = req.query;  //리퀘스트로 받아온(주소창에서 가져온 = query)id값
   // let body = [...parsedData.list].filter((obj) => obj.id != id); //아이디가 같지 않은 데이터만 추출 = 아이디가 같은 것만 삭제하는 효과!
@@ -102,13 +132,13 @@ todos.delete('/', async function (req, res) {
   // console.log(req.query);
   // // res.send(req.body)  //수정한 데이터만 보내줌
   // res.send({list:body});  //수정된 전체 data.json 보내줌
-//----------------------------------------------------------------------
-
+//--------------------------------------------------------
+//몽고db : deleteOne
   let id = req.query;  //리퀘스트로 받아온(주소창에서 가져온 = query)id값
   const collection = await mongoConnect();
   await collection.deleteOne(id);  //데이터 삭제{찾을id}
   const findResult = await collection.find({}).toArray();  //다시 데이터 가져와서 저장
-  client.close(); //서버 끊기
+  // client.close(); //서버 끊기
   res.send(findResult); //서버로 보내기
 })
 
